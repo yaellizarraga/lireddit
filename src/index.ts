a965b0c4b4b2941b0db0fe5903c6c499af358a12
Yael Lizarraga <yaellizarraga@outlook.es>
2020-09-02 21:11:24 -0600

    feature/initial commit

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import { MikroORM } from '@mikro-orm/core';
+import { __prod__ } from './constants';
+import { Post } from './entities/Posts';
+import microConfig from './mikro-orm.config';
+
+const main = async () => {
+  const orm = await MikroORM.init(microConfig);
+
+  const post = orm.em.create(Post, { title: 'My first post'});
+  await orm.em.persistAndFlush(post);
+};
+
+main();
---
+++ src/mikro-orm.config.ts
@@ -0,0 +1,15 @@
+import { MikroORM } from '@mikro-orm/core';
+import { Post } from "./entities/Posts";
+import { __prod__ } from "./constants";
+import path from 'path';
+
+export default {
+  migrations: {
+    path: path.join('./migrations'), // path to the folder with migrations
+    pattern: /^[\w-]+\d+\.[tj]s$/, // regex pattern for the migration files
+  },
+  entities: [Post],
+  dbName: 'lireddit',
+  type: 'postgresql',
+  debug: !__prod__
+} as Parameters<typeof MikroORM.init>[0];
